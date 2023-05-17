9823144fad42f53adc92858dbcebca6a71ed3f98
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-05-17 15:22:15 -0300

    feat: add eslint config

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+const tasks = [
+  { id: 1, titulo: "Aprender React", concluida: true },
+  { id: 2, titulo: "Estudar NodeJS", concluida: false },
+  { id: 3, titulo: "Praticar TypeScript", concluida: false },
+];
+
+export { tasks };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import express, { type Express, type Request, type Response } from 'express'
+
+const app: Express = express()
+const port = 5000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
